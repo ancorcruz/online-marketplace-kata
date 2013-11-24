34cd9b48e375bd558463d617be81e7a2efb87265
Ancor Cruz <hello@ancorcruz.com>
2013-11-24 17:51:19 +0000

    Added product

--- product.rb
+++ product.rb
@@ -1,2 +1,13 @@
 class Product
+  attr_reader :code, :name, :price_cents
+
+  def initialize code, name, price
+    @code        = code
+    @name        = name
+    @price_cents = (price * 100).round
+  end
+
+  def price
+    @price_cents / 100.0
+  end
 end
